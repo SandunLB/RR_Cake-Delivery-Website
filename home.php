225fbcbcb4ab91daa93732e4f06031ef9968bf38
SandunLB <sandunlb2001@gmail.com>
2024-05-07 18:29:27 +0530

    background image implimented !

--- home.php
+++ home.php
@@ -151,7 +151,7 @@ if (isset($_POST['add_to_cart'])) {
       <div class="box-container">
 
          <?php
-         $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
+         $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 9") or die('query failed');
          if (mysqli_num_rows($select_products) > 0) {
             while ($fetch_products = mysqli_fetch_assoc($select_products)) {
          ?>
